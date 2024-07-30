0d3fe4255e7794ae771fea78c286e4c26c6f0395
Shelley Gao <syugao@gmail.com>
2024-07-29 19:28:28 -0700

    revise test_built for more flexible snapshots

--- tests/test_build.py
+++ tests/test_build.py
@@ -9,20 +9,37 @@ from ref_builder.build import build_json
 
 
 def test_ok(
-    scratch_path: Path,
+    scratch_repo,
     snapshot: SnapshotAssertion,
     tmp_path: Path,
 ):
     """Test that the command exits with a 0 exit code."""
     output_path = tmp_path / "reference.json"
 
-    build_json(False, output_path, scratch_path, "2.1.0")
+    build_json(False, output_path, scratch_repo.path, "2.1.0")
 
     with open(output_path) as f:
         built_json = json.load(f)
 
-    assert built_json == snapshot(exclude=props("created_at"))
+    assert built_json == snapshot(exclude=props("created_at", "otus"))
     assert built_json["name"] == "2.1.0"
+
+    for built_otu in built_json["otus"]:
+        repo_otu = scratch_repo.get_otu(built_otu["_id"])
+        assert repo_otu is not None
+
+        assert built_otu == snapshot(exclude=props("_id", "isolates"))
+
+        for built_isolate in built_otu["isolates"]:
+            repo_isolate = repo_otu.get_isolate(built_isolate["id"])
+            assert repo_isolate is not None
+
+            assert built_isolate == snapshot(exclude=props("id", "sequences"))
+
+            for built_sequence in built_isolate["sequences"]:
+                assert built_sequence["_id"]
+                assert built_sequence == snapshot(exclude=props("_id"))
+
     assert (arrow.utcnow() - arrow.get(built_json["created_at"])).seconds == 0
 
 
