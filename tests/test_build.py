f45b22e3c9685cd7f82fa76457841bab1d5f2dfb
Shelley Gao <syugao@gmail.com>
2024-07-29 19:56:59 -0700

    Fix some details in test_build
    remove problematic repr_isolate from build
    
    comparison_reference fixture
    
    fix comparison_reference

--- tests/test_build.py
+++ tests/test_build.py
@@ -2,43 +2,52 @@ import json
 from pathlib import Path
 
 import arrow
+import pytest
+import orjson
 from syrupy import SnapshotAssertion
 from syrupy.filters import props
 
 from ref_builder.build import build_json
 
 
+@pytest.fixture
+def comparison_reference(pytestconfig, tmp_path, scratch_repo) -> dict:
+    comparison_reference = pytestconfig.cache.get("comparison_reference", None)
+
+    if comparison_reference:
+        return comparison_reference
+
+    output_path = tmp_path / "comparison_reference.json"
+    build_json(False, output_path, scratch_repo.path, "2.1.0")
+
+    with open(output_path, "rb") as f:
+        comparison_reference = orjson.loads(f.read())
+
+    if comparison_reference:
+        pytestconfig.cache.set("comparison_reference", comparison_reference)
+        return comparison_reference
+
+    raise ValueError("Could not build comparison reference")
+
+
 def test_ok(
-    scratch_repo,
-    snapshot: SnapshotAssertion,
-    tmp_path: Path,
+    scratch_repo, comparison_reference, tmp_path: Path, snapshot: SnapshotAssertion
 ):
     """Test that the command exits with a 0 exit code."""
     output_path = tmp_path / "reference.json"
 
     build_json(False, output_path, scratch_repo.path, "2.1.0")
 
-    with open(output_path) as f:
-        built_json = json.load(f)
+    with open(output_path, "rb") as f:
+        built_json = orjson.loads(f.read())
+
+    assert comparison_reference
+
+    assert built_json
 
     assert built_json == snapshot(exclude=props("created_at", "otus"))
-    assert built_json["name"] == "2.1.0"
 
-    for built_otu in built_json["otus"]:
-        repo_otu = scratch_repo.get_otu(built_otu["_id"])
-        assert repo_otu is not None
-
-        assert built_otu == snapshot(exclude=props("_id", "isolates"))
-
-        for built_isolate in built_otu["isolates"]:
-            repo_isolate = repo_otu.get_isolate(built_isolate["id"])
-            assert repo_isolate is not None
-
-            assert built_isolate == snapshot(exclude=props("id", "sequences"))
-
-            for built_sequence in built_isolate["sequences"]:
-                assert built_sequence["_id"]
-                assert built_sequence == snapshot(exclude=props("_id"))
+    assert built_json["otus"] == comparison_reference["otus"]
 
     assert (arrow.utcnow() - arrow.get(built_json["created_at"])).seconds == 0
 
