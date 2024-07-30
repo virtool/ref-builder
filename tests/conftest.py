ad40f769ccde74f513a081c09acf8075f9410617
Shelley Gao <syugao@gmail.com>
2024-07-30 16:03:09 -0700

    Add generation script to scratch_event_store_data fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
 import shutil
 from pathlib import Path
+from typing import List
 
 import orjson
 import pytest
+from pydantic import BaseModel, TypeAdapter
 from pytest_mock import MockerFixture
 
 from ref_builder.legacy.utils import build_legacy_otu
 from ref_builder.ncbi.cache import NCBICache
 from ref_builder.ncbi.client import NCBIClient
 from ref_builder.repo import Repo
+from ref_builder.otu import create_otu_with_schema, add_sequences
 from ref_builder.utils import DataType
-from tests.src_test_gen import generate_src_test_files
 
 
 @pytest.fixture()
@@ -146,7 +148,46 @@ def scratch_event_store_data(pytestconfig, tmp_path, scratch_repo_contents_path)
     """Scratch repo events. Cached in .pytest_cache."""
     scratch_src = pytestconfig.cache.get("scratch_src", None)
     if scratch_src is None:
-        scratch_src = generate_src_test_files(tmp_path, scratch_repo_contents_path)
+        temp_scratch_repo = Repo.new(
+            data_type=DataType.GENOME, name="src_test", path=tmp_path, organism="viruses"
+        )
+
+        with open(scratch_repo_contents_path, "rb") as f:
+            toc = otu_contents_list_adapter.validate_json(f.read())
+
+        for otu_contents in toc:
+            otu = create_otu_with_schema(
+                repo=temp_scratch_repo,
+                taxid=otu_contents.taxid,
+                accessions=otu_contents.otu_schema
+            )
+
+            add_sequences(
+                repo=temp_scratch_repo,
+                otu=otu,
+                accessions=otu_contents.contents
+            )
+
+        scratch_src = {}
+        for event_file_path in (tmp_path / "src").glob("*.json"):
+            with open(event_file_path, "r") as f:
+                scratch_src[event_file_path.name] = orjson.loads(f.read())
+
         pytestconfig.cache.set("scratch_src", scratch_src)
 
     return scratch_src
+
+
+class OTUContents(BaseModel):
+    taxid: int
+    """The Taxonomy ID of the OTU."""
+
+    otu_schema: list[str]
+    """A list of accessions that determine the OTU's schema."""
+
+    contents: list[str]
+    """A list of accessions to be added to the OTU."""
+
+
+otu_contents_list_adapter = TypeAdapter(List[OTUContents])
+"""Aids the serialization of a scratch repo's table of contents."""
