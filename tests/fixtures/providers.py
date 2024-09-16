6eab2bbe1c01f7b4171d0f7550884935883d98ac
Shelley Gao <syugao@gmail.com>
2024-09-16 13:53:11 -0700

    add AccessionProvider, SequenceProvider

--- tests/fixtures/providers.py
+++ tests/fixtures/providers.py
@@ -1,5 +1,8 @@
+from collections import OrderedDict
+
 from faker.providers import BaseProvider, DynamicProvider
 
+from ref_builder.utils import Accession
 from tests.fixtures.data.organism import (
     ORGANISM_HOSTS,
     ORGANISM_PARTS,
@@ -12,9 +15,57 @@ from tests.fixtures.data.organism import (
 )
 
 
-SEQUENCE_CHARACTERS = 'ATCGRYKMSWBDHVN'
+UNCOMMON_PROBABILITY = 0.01
+SEQUENCE_DICTIONARY = OrderedDict([
+    ("A", 0.245),
+    ("T", 0.245),
+    ("C", 0.245),
+    ("G", 0.245),
+    ("R", UNCOMMON_PROBABILITY),
+    ("Y", UNCOMMON_PROBABILITY),
+    ("K", UNCOMMON_PROBABILITY),
+    ("M", UNCOMMON_PROBABILITY),
+    ("S", UNCOMMON_PROBABILITY),
+    ("W", UNCOMMON_PROBABILITY),
+    ("B", UNCOMMON_PROBABILITY),
+    ("D", UNCOMMON_PROBABILITY),
+    ("H", UNCOMMON_PROBABILITY),
+    ("V", UNCOMMON_PROBABILITY),
+    ("N", UNCOMMON_PROBABILITY),
+])
 SEQUENCE_MIN = 100
 SEQUENCE_MAX = 1500
+
+
+class AccessionProvider(BaseProvider):
+    def refseq_accession(self):
+        return "NC_" + self.numerify("######")
+
+    def genbank_accession(self):
+        dice_roll = self.random_int(0, 10)
+
+        if dice_roll > 6:
+            return self.bothify("?#####").upper()
+
+        return self.bothify("??######").upper()
+
+    def accession(self) -> str:
+        dice_roll = self.random_int(0, 10)
+
+        if dice_roll > 7:
+            return self.genbank_accession()
+        else:
+            return self.refseq_accession()
+
+
+class SequenceProvider(BaseProvider):
+    def sequence(self):
+        return "".join(
+            self.random_elements(
+                SEQUENCE_DICTIONARY,
+                self.random_int(SEQUENCE_MIN, SEQUENCE_MAX),
+                use_weighting=True,
+        ))
 
 
 class OrganismProvider(BaseProvider):
@@ -116,7 +167,13 @@ if __name__ == '__main__':
     fake = Faker()
     fake.add_provider(SourceProvider)
     fake.add_provider(OrganismProvider)
+    fake.add_provider(SequenceProvider)
+    fake.add_provider(AccessionProvider)
 
     for _ in range(10):
         print(fake.segment())
         print(fake.organism())
+        print(fake.sequence())
+        print(fake.accession())
+        print()
+
