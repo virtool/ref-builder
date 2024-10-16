8a16bbee032189a10924a3ce1e5e881537f8ba58
Shelley Gao <syugao@gmail.com>
2024-10-16 14:35:57 -0700

    clean up events.loader imports

--- ref_builder/events/loader.py
+++ ref_builder/events/loader.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-from enum import StrEnum
 from typing import Annotated, Union
 
-from pydantic import BaseModel, Field, TypeAdapter
+from pydantic import Field, TypeAdapter
 
 from ref_builder.events.repo import CreateRepo
 from ref_builder.events.otu import (
@@ -13,19 +11,6 @@ from ref_builder.events.otu import (
 )
 from ref_builder.events.isolate import CreateIsolate, LinkSequence, DeleteIsolate
 from ref_builder.events.sequence import CreateSequence, DeleteSequence
-
-
-class EventType(StrEnum):
-    CREATE_ISOLATE = "CreateIsolate"
-    CREATE_OTU = "CreateOTU"
-    CREATE_PLAN = "CreatePlan"
-    CREATE_REPO = "CreateRepo"
-    CREATE_SEQUENCE = "CreateSequence"
-    DELETE_ISOLATE = "DeleteIsolate"
-    DELETE_SEQUENCE = "DeleteSequence"
-    EXCLUDE_ACCESSION = "ExcludeAccession"
-    LINK_SEQUENCE = "LinkSequence"
-    SET_REPR_ISOLATE = "SetReprIsolate"
 
 
 LoadableEvent = Annotated[
