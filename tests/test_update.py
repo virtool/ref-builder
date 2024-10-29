53e4093142f185c061b5877b8656cf840ccddf10
Shelley Gao <syugao@gmail.com>
2024-10-29 13:02:36 -0700

    update test_ok() to evaluate exclusion behaviour

--- tests/test_update.py
+++ tests/test_update.py
@@ -10,38 +10,44 @@ from ref_builder.repo import Repo
 class TestUpdateOTU:
     """Test automatic OTU update functionality."""
 
-    def test_without_exclusions_ok(
+    def test_ok(
         self,
         precached_repo: Repo,
         snapshot: SnapshotAssertion,
     ):
-        """"""
+        """Test automatic update behaviour."""
         otu_before = create_otu(
             precached_repo,
             2164102,
-            ["MF062136", "MF062137", "MF062138"],
+            ["NC_055390", "NC_055391", "NC_055392"],
             "",
         )
 
-        assert otu_before.accessions == {"MF062136", "MF062137", "MF062138"}
+        assert otu_before.accessions == {"NC_055390", "NC_055391", "NC_055392"}
+
+        assert otu_before.blocked_accessions == {
+            "NC_055390",
+            "NC_055391",
+            "NC_055392",
+        }.union({"MF062125", "MF062126", "MF062127"})
 
         auto_update_otu(precached_repo, otu_before)
 
         otu_after = precached_repo.get_otu(otu_before.id)
 
+        assert otu_after.excluded_accessions == {"MF062125", "MF062126", "MF062127"}
+
         assert otu_after.id == otu_before.id
 
         assert otu_after.isolate_ids.issuperset(otu_before.isolate_ids)
 
-        assert otu_after.excluded_accessions == {"MF062125", "MF062126", "MF062127"}
-
         assert otu_after.accessions == {
-            "MF062136",
-            "MF062137",
-            "MF062138",
             "MF062130",
             "MF062131",
             "MF062132",
+            "MF062136",
+            "MF062137",
+            "MF062138",
             "MK936225",
             "MK936226",
             "MK936227",
@@ -57,14 +63,15 @@ class TestUpdateOTU:
         }
 
         assert {
-           str(isolate.name): isolate.accessions for isolate in otu_after.isolates
+            str(isolate.name): isolate.accessions for isolate in otu_after.isolates
         } == snapshot()
 
-    def test_with_replacement_ok(
+    def test_with_refseq_replacement_ok(
         self,
         precached_repo: Repo,
         snapshot: SnapshotAssertion,
     ):
+        """Test that automatic update replaces superceded accessions with RefSeq versions."""
         otu_before = create_otu(
             precached_repo,
             2164102,
