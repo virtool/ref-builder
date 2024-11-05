05b69b0d0a78a8f02b86b56e0986f7201af2add2
Shelley Gao <syugao@gmail.com>
2024-11-05 13:45:38 -0800

    add TestAddMonopartiteIsolate

--- tests/ref/test_inclusion_checks.py
+++ tests/ref/test_inclusion_checks.py
@@ -1,12 +1,33 @@
 import pytest
 from faker import Faker
 
-from ref_builder.otu.utils import check_sequence_length
-from tests.fixtures.providers import SequenceProvider
+from ref_builder.resources import RepoOTU
+from ref_builder.ncbi.models import NCBIGenbank
+from ref_builder.otu.update import create_isolate_from_records
+from ref_builder.otu.utils import check_sequence_length, IsolateName, IsolateNameType
+from tests.fixtures.factories import NCBIGenbankFactory, NCBISourceFactory
+from tests.fixtures.providers import AccessionProvider, SequenceProvider
 
 
 faker = Faker()
+faker.add_provider(AccessionProvider)
 faker.add_provider(SequenceProvider)
+
+
+def create_mock_record(
+    otu: RepoOTU, sequence_length: int,
+) -> NCBIGenbank:
+    mock_record_source = NCBISourceFactory.build(
+        taxid=otu.taxid, organism=otu.name,
+    )
+
+    mock_record = NCBIGenbankFactory.build(
+        accession=faker.genbank_accession(),
+        source=mock_record_source,
+        sequence=faker.sequence(min=sequence_length, max=sequence_length),
+    )
+
+    return mock_record
 
 
 class TestSequenceLengthCheck:
@@ -50,3 +71,66 @@ class TestSequenceLengthCheck:
             fake_sequence = faker.sequence(min=sequence_length, max=sequence_length)
 
             assert not check_sequence_length(fake_sequence, 1000, tolerance)
+
+
+class TestAddMonopartiteIsolate:
+    """Test that new monopartite isolates follow plan length limits."""
+
+    @pytest.mark.parametrize(
+        "sequence_length_multiplier",
+        [1.0, 1.03, 0.98]
+    )
+    def test_ok(self, scratch_repo, sequence_length_multiplier: float):
+        """Test that sequences within recommended length variance
+        are added without issue."""
+        otu_before = scratch_repo.get_otu_by_taxid(270478)
+
+        mock_record = create_mock_record(
+            otu_before,
+            int(otu_before.plan.length * sequence_length_multiplier),
+        )
+
+        assert mock_record
+
+        mock_isolate = create_isolate_from_records(
+            scratch_repo,
+            otu_before,
+            isolate_name=IsolateName(type=IsolateNameType.ISOLATE, value="mock"),
+            records=[mock_record],
+        )
+
+        otu_after = scratch_repo.get_otu_by_taxid(270478)
+
+        assert mock_isolate.id in otu_after.isolate_ids
+
+        assert mock_record.accession in otu_after.accessions
+
+    @pytest.mark.parametrize(
+        "sequence_length_multiplier",
+        [0.5, 1.035, 20.0]
+    )
+    def test_fail(self, scratch_repo, sequence_length_multiplier: float):
+        """Test that sequences that exceed recommended length variance
+        are automatically rejected.
+        """
+        otu_before = scratch_repo.get_otu_by_taxid(270478)
+
+        mock_record = create_mock_record(
+            otu_before,
+            int(otu_before.plan.length * sequence_length_multiplier),
+        )
+
+        assert mock_record
+
+        mock_isolate = create_isolate_from_records(
+            scratch_repo,
+            otu_before,
+            isolate_name=IsolateName(type=IsolateNameType.ISOLATE, value="mock"),
+            records=[mock_record],
+        )
+
+        assert mock_isolate is None
+
+        otu_after = scratch_repo.get_otu_by_taxid(270478)
+
+        assert mock_record.accession not in otu_after.accessions
