f9c81eb6086e33235a1ffcf21629fd511d0d8109
Shelley Gao <syugao@gmail.com>
2024-12-09 16:12:59 -0800

    remove parametrization from factory tests, loop within test instead

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -2,11 +2,8 @@
 
 from uuid import uuid4
 
-import pytest
-
-from ref_builder.ncbi.models import NCBIGenbank, NCBISource
+from ref_builder.ncbi.models import NCBISource
 from ref_builder.resources import RepoIsolate, RepoOTU, RepoSequence
-from ref_builder.otu.models import IsolateBase, OTUBase
 from ref_builder.utils import Accession
 from tests.fixtures.factories import (
     IsolateFactory,
@@ -18,44 +15,43 @@ from tests.fixtures.factories import (
 
 
 class TestNCBIFactories:
-    @pytest.mark.parametrize("dummy_source", NCBISourceFactory.coverage(10))
-    def test_source_factory(self, dummy_source: NCBISource):
-        """Test that NCBISourceFactory creates valid input."""
-        assert dummy_source
-
-    @pytest.mark.parametrize("dummy_record", NCBIGenbankFactory.coverage(10))
-    def test_genbank_factory(self, dummy_record: NCBIGenbank):
-        """Test that NCBISourceFactory creates valid input."""
-        assert dummy_record
-
-        assert RepoSequence(
-            id=uuid4(),
-            accession=Accession.from_string(dummy_record.accession_version),
-            definition=dummy_record.definition,
-            legacy_id=None,
-            segment=dummy_record.source.segment,
-            sequence=dummy_record.sequence,
+    def test_source_factory(self):
+        """Test that NCBISourceFactory creates valid mock NCBISource objects."""
+        assert all(
+            [
+                isinstance(dummy_source, NCBISource)
+                for dummy_source in NCBISourceFactory.coverage()
+            ]
         )
 
+    def test_genbank_factory(self):
+        """Test that NCBIGenbankFactory creates mock NCBIGenbank records
+        and ensure said records are valid RepoSequence input."""
 
-@pytest.mark.parametrize("dummy_sequence", SequenceFactory.coverage(10))
-def test_sequence_factory(dummy_sequence: RepoSequence):
-    assert dummy_sequence
-
-    print(dummy_sequence)
-
-    assert RepoSequence.model_validate(dummy_sequence.model_dump())
-
-
-@pytest.mark.parametrize("dummy_isolate", IsolateFactory.coverage(5))
-def test_isolate_factory(dummy_isolate: IsolateBase):
-    assert dummy_isolate
-
-    assert RepoIsolate.model_validate(dummy_isolate.model_dump())
+        for dummy_record in NCBIGenbankFactory.coverage():
+            assert RepoSequence(
+                id=uuid4(),
+                accession=Accession.from_string(dummy_record.accession_version),
+                definition=dummy_record.definition,
+                legacy_id=None,
+                segment=dummy_record.source.segment,
+                sequence=dummy_record.sequence,
+            )
 
 
-@pytest.mark.parametrize("dummy_otu", OTUFactory.coverage(10))
-def test_otu_factory(dummy_otu: OTUBase):
-    assert dummy_otu
+def test_sequence_factory():
+    """Test that SequenceFactory creates valid mock sequence data."""
+    for dummy_sequence in SequenceFactory.coverage():
+        assert RepoSequence.model_validate(dummy_sequence.model_dump())
 
-    assert RepoOTU.model_validate(dummy_otu.model_dump())
+
+def test_isolate_factory():
+    """Test that IsolateFactory creates valid mock isolate data."""
+    for dummy_isolate in IsolateFactory.coverage():
+        assert RepoIsolate.model_validate(dummy_isolate.model_dump())
+
+
+def test_otu_factory():
+    """Test that OTUFactory creates valid mock OTU data."""
+    for dummy_otu in OTUFactory.coverage():
+        assert RepoOTU.model_validate(dummy_otu.model_dump())
