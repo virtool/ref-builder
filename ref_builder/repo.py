f2d18435e883f4a07a95a97639ce3f547773775f
Shelley Gao <syugao@gmail.com>
2024-12-19 15:34:28 -0800

    add EditAllowedAccessions to EventStore.read_event()

--- ref_builder/repo.py
+++ ref_builder/repo.py
@@ -673,6 +673,7 @@ class EventStore:
                     "CreatePlan": CreatePlan,
                     "SetReprIsolate": SetReprIsolate,
                     "ExcludeAccession": ExcludeAccession,
+                    "EditAllowedAccessions": EditAllowedAccessions,
                 }[loaded["type"]]
 
                 return cls(**loaded)
