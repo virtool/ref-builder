a41f3316f55caad7591201d0024b4922c12f20d8
Shelley Gao <syugao@gmail.com>
2025-03-05 10:28:23 -0800

    move TestCreateOTUCommands

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,3 @@
-import subprocess
-from pathlib import Path
-
 import pytest
 from click.testing import CliRunner
 from syrupy import SnapshotAssertion
@@ -21,29 +18,6 @@ from ref_builder.otu.update import (
 from ref_builder.otu.utils import RefSeqConflictError
 from ref_builder.repo import Repo
 from ref_builder.utils import IsolateName, IsolateNameType
-
-
-def run_create_otu_command(
-    path: Path,
-    taxid: int,
-    accessions: list,
-    acronym: str = "",
-):
-    subprocess.run(
-        [
-            "ref-builder",
-            "otu",
-            "--path",
-            str(path),
-            "create",
-            *accessions,
-            "--taxid",
-            str(taxid),
-            "--acronym",
-            acronym,
-        ],
-        check=False,
-    )
 
 
 class TestCreateOTU:
@@ -193,107 +167,6 @@ class TestCreateOTU:
         otu = precached_repo.get_otu_by_taxid(expected_taxid)
 
         assert otu is not None
-
-
-class TestCreateOTUCommands:
-    @pytest.mark.parametrize(
-        ("taxid", "accessions"),
-        [(1278205, ["NC_020160"]), (345184, ["DQ178610", "DQ178611"])],
-    )
-    def test_ok(
-        self,
-        taxid: int,
-        accessions: list[str],
-        precached_repo: Repo,
-        snapshot: SnapshotAssertion,
-    ):
-        """Test that an OTU can be created using the command line interface.
-
-        Also check resulting print_otu() console output.
-        """
-        runner = CliRunner()
-
-        result = runner.invoke(
-            otu_command_group,
-            ["--path", str(precached_repo.path)]
-            + ["create", "--taxid", str(taxid)]
-            + accessions,
-        )
-
-        assert result.exit_code == 0
-
-        otus = list(Repo(precached_repo.path).iter_otus())
-
-        assert len(otus) == 1
-        assert otus[0].model_dump() == snapshot(
-            exclude=props("id", "isolates", "representative_isolate"),
-        )
-
-        with console.capture() as capture:
-            print_otu(otus[0])
-
-        assert capture.get() in result.output
-
-    @pytest.mark.parametrize(
-        "taxid, accessions",
-        [(1278205, ["NC_020160"]), (345184, ["DQ178610", "DQ178611"])],
-    )
-    def test_without_taxid_ok(
-        self,
-        taxid: int,
-        accessions: list[str],
-        precached_repo: Repo,
-    ):
-        subprocess.run(
-            [
-                "ref-builder",
-                "otu",
-                "--path",
-                str(precached_repo.path),
-                "create",
-                *accessions,
-            ],
-            check=False,
-        )
-
-        otus = list(Repo(precached_repo.path).iter_otus())
-
-        assert len(otus) == 1
-        assert otus[0].taxid == taxid
-
-    def test_acronym(self, precached_repo: Repo):
-        """Test if the --acronym option works as planned."""
-        run_create_otu_command(
-            taxid=345184,
-            accessions=["DQ178610", "DQ178611"],
-            path=precached_repo.path,
-            acronym="CabLCJV",
-        )
-
-        otus = list(Repo(precached_repo.path).iter_otus())
-
-        assert len(otus) == 1
-        assert otus[0].acronym == "CabLCJV"
-
-    def test_duplicate_accessions(self, precached_repo: Repo):
-        """Test that an error is raised when duplicate accessions are provided."""
-        runner = CliRunner()
-
-        result = runner.invoke(
-            otu_command_group,
-            [
-                "--path",
-                str(precached_repo.path),
-                "create",
-                "--taxid",
-                str(1169032),
-                "MK431779",
-                "MK431779",
-            ],
-        )
-
-        assert result.exit_code == 2
-        assert "Duplicate accessions are not allowed." in result.output
 
 
 class TestAddIsolate:
