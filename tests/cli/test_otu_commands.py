167bde3f8db32011ee7055d9ed92c1fb261786ad
Shelley Gao <syugao@gmail.com>
2025-03-05 10:35:58 -0800

    refactor TestCreateOTUCommand
    update ambr

--- tests/cli/test_otu_commands.py
+++ tests/cli/test_otu_commands.py
@@ -11,47 +11,27 @@ from ref_builder.console import console, print_otu
 from ref_builder.repo import Repo
 
 
-def run_create_otu_command(
-    path: Path,
-    taxid: int,
-    accessions: list,
-    acronym: str = "",
-):
-    subprocess.run(
-        [
-            "ref-builder",
-            "otu",
-            "--path",
-            str(path),
-            "create",
-            *accessions,
-            "--taxid",
-            str(taxid),
-            "--acronym",
-            acronym,
-        ],
-        check=False,
-    )
+runner = CliRunner()
 
 
 class TestCreateOTUCommands:
+    """Test the behaviour of ``ref-builder otu create``."""
+
     @pytest.mark.parametrize(
         ("taxid", "accessions"),
         [(1278205, ["NC_020160"]), (345184, ["DQ178610", "DQ178611"])],
     )
-    def test_ok(
+    def test_with_taxid_ok(
         self,
         taxid: int,
         accessions: list[str],
         precached_repo: Repo,
         snapshot: SnapshotAssertion,
     ):
-        """Test that an OTU can be created using the command line interface.
+        """Test that an OTU can be created with the --taxid option.
 
         Also check resulting print_otu() console output.
         """
-        runner = CliRunner()
-
         result = runner.invoke(
             otu_command_group,
             ["--path", str(precached_repo.path)]
@@ -74,7 +54,7 @@ class TestCreateOTUCommands:
         assert capture.get() in result.output
 
     @pytest.mark.parametrize(
-        "taxid, accessions",
+        ("taxid", "accessions"),
         [(1278205, ["NC_020160"]), (345184, ["DQ178610", "DQ178611"])],
     )
     def test_without_taxid_ok(
@@ -83,17 +63,13 @@ class TestCreateOTUCommands:
         accessions: list[str],
         precached_repo: Repo,
     ):
-        subprocess.run(
-            [
-                "ref-builder",
-                "otu",
-                "--path",
-                str(precached_repo.path),
-                "create",
-                *accessions,
-            ],
-            check=False,
+        """Test that an OTU can be created without the --taxid option."""
+        result = runner.invoke(
+            otu_command_group,
+            ["--path", str(precached_repo.path)] + ["create"] + accessions,
         )
+
+        assert result.exit_code == 0
 
         otus = list(Repo(precached_repo.path).iter_otus())
 
@@ -101,18 +77,28 @@ class TestCreateOTUCommands:
         assert otus[0].taxid == taxid
 
     def test_acronym(self, precached_repo: Repo):
-        """Test if the --acronym option works as planned."""
-        run_create_otu_command(
-            taxid=345184,
-            accessions=["DQ178610", "DQ178611"],
-            path=precached_repo.path,
-            acronym="CabLCJV",
+        """Test that Athe --acronym option works as planned."""
+        acronym_ = "CabLCJV"
+
+        result = runner.invoke(
+            otu_command_group,
+            ["--path", str(precached_repo.path)]
+            + [
+                "create",
+                "--taxid",
+                str(345184),
+                "--acronym",
+                acronym_,
+            ]
+            + ["DQ178610", "DQ178611"],
         )
+
+        assert result.exit_code == 0
 
         otus = list(Repo(precached_repo.path).iter_otus())
 
         assert len(otus) == 1
-        assert otus[0].acronym == "CabLCJV"
+        assert otus[0].acronym == acronym_
 
     def test_duplicate_accessions(self, precached_repo: Repo):
         """Test that an error is raised when duplicate accessions are provided."""
@@ -120,15 +106,9 @@ class TestCreateOTUCommands:
 
         result = runner.invoke(
             otu_command_group,
-            [
-                "--path",
-                str(precached_repo.path),
-                "create",
-                "--taxid",
-                str(1169032),
-                "MK431779",
-                "MK431779",
-            ],
+            ["--path", str(precached_repo.path)]
+            + ["create", "--taxid", str(1169032)]
+            + ["MK431779", "MK431779"],
         )
 
         assert result.exit_code == 2
