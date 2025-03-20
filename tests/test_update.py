210384b89831caae6338dd83da2e1c9aa10c7682
Shelley Gao <syugao@gmail.com>
2025-03-20 13:07:33 -0700

    test batch update recent checks

--- tests/test_update.py
+++ tests/test_update.py
@@ -288,15 +288,18 @@ class TestBatchUpdate:
         mock_repo: Repo,
         mock_fetch_index: dict[int, set[str]],
     ):
-        """Test that batch-update works as expected."""
+        """Test that batch update works as expected."""
         otu_initial = next(mock_repo.iter_otus())
 
         with mock_repo.lock():
-            batch_update_repo(mock_repo)
+            assert len(batch_update_repo(mock_repo)) == 1
 
         otu_after = next(mock_repo.iter_otus())
 
         assert otu_after.accessions == mock_fetch_index[otu_initial.taxid]
+
+        with mock_repo.lock():
+            assert len(batch_update_repo(mock_repo)) == 0
 
     def test_with_fetch_index_ok(
         self,
@@ -308,14 +311,18 @@ class TestBatchUpdate:
         otu_initial = next(mock_repo.iter_otus())
 
         with mock_repo.lock():
-            batch_update_repo(mock_repo, fetch_index_path=mock_fetch_index_path)
+            assert len(
+                batch_update_repo(mock_repo, fetch_index_path=mock_fetch_index_path)
+            ) == 1
 
         otu_after = next(mock_repo.iter_otus())
 
         assert otu_after.accessions == mock_fetch_index[otu_initial.taxid]
 
         with mock_repo.lock():
-            batch_update_repo(mock_repo, fetch_index_path=mock_fetch_index_path)
+            assert len(
+                batch_update_repo(mock_repo, fetch_index_path=mock_fetch_index_path)
+            ) == 0
 
 
 def test_iter_fetch_list(
