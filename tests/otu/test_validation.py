1d0aa0ba4ad4cccf11cd74781564fa0e1936d09a
Shelley Gao <syugao@gmail.com>
2025-03-25 14:47:03 -0700

    add TestValidation.test_no_isolates

--- tests/otu/test_validation.py
+++ tests/otu/test_validation.py
@@ -9,3 +9,16 @@ class TestValidateOTU:
         """Validate scratch_repo contents"""
         for otu_metadata in scratch_repo.iter_minimal_otus():
             assert validate_otu(scratch_repo.get_otu_by_taxid(otu_metadata.taxid))
+
+    def test_no_isolates(self, scratch_repo: Repo):
+        """Test that validating an OTU with no isolates returns False."""
+        otu_clean = scratch_repo.get_otu_by_taxid(223262)
+
+        assert validate_otu(otu_clean)
+
+        otu_invalid = otu_clean.model_copy()
+        otu_invalid.delete_isolate(otu_clean.representative_isolate)
+
+        assert not otu_invalid.isolates
+
+        assert not validate_otu(otu_invalid)
