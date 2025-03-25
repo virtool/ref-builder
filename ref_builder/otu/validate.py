d874caad0ffdd025f2b886e78e3925ac829586b8
Shelley Gao <syugao@gmail.com>
2025-03-25 15:25:36 -0700

    add get_validated_otu()

--- ref_builder/otu/validate.py
+++ ref_builder/otu/validate.py
@@ -11,6 +11,22 @@ from ref_builder.otu.models import OTU
 logger = get_logger("otu.validate")
 
 
+def get_validated_otu(unvalidated_otu: RepoOTU) -> OTU:
+    """Get a validated OTU from a RepoOTU instance."""
+    return OTU(
+        id=unvalidated_otu.id,
+        legacy_id=unvalidated_otu.legacy_id,
+        name=unvalidated_otu.name,
+        taxid=unvalidated_otu.taxid,
+        acronym=unvalidated_otu.acronym,
+        molecule=unvalidated_otu.molecule,
+        plan=unvalidated_otu.plan,
+        isolates=unvalidated_otu.isolates,
+        excluded_accessions=unvalidated_otu.excluded_accessions,
+        representative_isolate=unvalidated_otu.representative_isolate,
+    )
+
+
 def validate_otu(unvalidated_otu: RepoOTU) -> bool:
     """Assure that an OTU can pass the validation standard."""
     try:
