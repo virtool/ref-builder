d4e9a1898a5a834c27b3b53579d466e8451ae898
Shelley Gao <syugao@gmail.com>
2025-03-27 13:49:40 -0700

    add replace_otu_sequences_from_record()

--- ref_builder/otu/promote.py
+++ ref_builder/otu/promote.py
@@ -5,7 +5,7 @@ from structlog import get_logger
 from ref_builder.ncbi.client import NCBIClient
 from ref_builder.ncbi.models import NCBIGenbank
 from ref_builder.repo import Repo
-from ref_builder.resources import RepoOTU
+from ref_builder.resources import RepoOTU, RepoSequence
 from ref_builder.otu.utils import (
     DeleteRationale,
     assign_segment_id_to_record,
@@ -192,3 +192,70 @@ def replace_accessions_from_records(
         )
 
     return replacement_sequence_ids
+
+
+def replace_otu_sequence_from_record(
+    repo: Repo,
+    otu: RepoOTU,
+    sequence_id: UUID,
+    replacement_record: NCBIGenbank,
+    exclude_accession: bool = True,
+) -> RepoSequence | None:
+    """Take the ID of a sequence and a GenBank record and replace the predecessor sequence
+    with a new sequence based on the record.
+    """
+    predecessor_sequence = otu.get_sequence_by_id(sequence_id)
+    if predecessor_sequence is None:
+        logger.warning("Predecessor sequences not found")
+
+    containing_isolate_ids = otu.get_isolate_ids_containing_sequence_id(
+        predecessor_sequence.id
+    )
+    if not containing_isolate_ids:
+        logger.info("Sequence id not found in any isolates.")
+        return None
+
+    logger.debug(
+        "Isolates containing sequence found",
+        replaceable_sequence=str(predecessor_sequence.id),
+        isolate_ids=[str(isolate_id) for isolate_id in containing_isolate_ids],
+    )
+
+    segment_id = assign_segment_id_to_record(replacement_record, otu.plan)
+    if segment_id is None:
+        logger.error("This segment does not match the plan.")
+        return None
+
+    with repo.use_transaction():
+        if otu.get_sequence_by_accession(replacement_record.accession) is None:
+            replacement_sequence = repo.create_sequence(
+                otu.id,
+                accession=replacement_record.accession_version,
+                definition=replacement_record.definition,
+                legacy_id=None,
+                segment=segment_id,
+                sequence=replacement_record.sequence,
+            )
+
+            if replacement_sequence is None:
+                logger.error("Isolate update failed when creating new sequence.")
+                return None
+
+        else:
+            replacement_sequence = otu.get_sequence_by_accession(
+                replacement_record.accession
+            )
+
+        for isolate_id in containing_isolate_ids:
+            repo.replace_sequence(
+                otu.id,
+                isolate_id,
+                replacement_sequence.id,
+                replaced_sequence_id=predecessor_sequence.id,
+                rationale=DeleteRationale.REFSEQ,
+            )
+
+        if exclude_accession:
+            repo.exclude_accession(otu.id, predecessor_sequence.accession.key)
+
+    return repo.get_otu(otu.id).get_sequence_by_id(replacement_sequence.id)
