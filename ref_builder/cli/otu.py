d0679cbfe6661b3495e113dff4e66a5ce32f7c7a
Shelley Gao <syugao@gmail.com>
2025-04-02 11:33:01 -0700

    deprecate _get_otu_id_from_other

--- ref_builder/cli/otu.py
+++ ref_builder/cli/otu.py
@@ -398,32 +398,3 @@ def plan_rename_segment(
         )
     except ValueError as e:
         click.echo(e, err=True)
-
-
-def _get_otu_id_from_other_identifier(repo: Repo, identifier: str) -> UUID | None:
-    """Return an OTU id from the repo if identifier matches a single OTU.
-    Return None if no matching OTU is found, raise a ValueError if >1 OTU is found.
-    """
-    try:
-        taxid = int(identifier)
-    except ValueError:
-        pass
-    else:
-        return repo.get_otu_id_by_taxid(taxid)
-
-    try:
-        return repo.get_otu_id_by_partial(identifier)
-
-    except PartialIDConflictError:
-        click.echo(
-            "Partial ID too short to narrow down results.",
-            err=True,
-        )
-
-    except InvalidInputError as e:
-        click.echo(
-            e,
-            err=True,
-        )
-
-    return None
