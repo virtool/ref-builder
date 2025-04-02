2eca6e22af03e05e15a6cf04b0e0ec6bdc050eec
Shelley Gao <syugao@gmail.com>
2025-04-02 11:24:15 -0700

    refactor cli.utils.get_otu_from_identifier()

--- ref_builder/cli/utils.py
+++ ref_builder/cli/utils.py
@@ -3,7 +3,7 @@ from uuid import UUID
 
 import click
 
-from ref_builder.errors import PartialIDConflictError, InvalidInputError
+from ref_builder.errors import InvalidInputError, PartialIDConflictError
 from ref_builder.repo import Repo
 from ref_builder.resources import RepoOTU
 
@@ -11,11 +11,44 @@ pass_repo = click.make_pass_decorator(Repo)
 
 
 def get_otu_from_identifier(repo: Repo, identifier: str) -> RepoOTU:
-    """Return an OTU from the given repo if identifier matches a single OTU."""
-    try:
-        otu_id = UUID(identifier)
-    except ValueError:
-        otu_id = _get_otu_id_from_other_identifier(repo, identifier)
+    """Return an OTU id from the repo if identifier matches a single OTU.
+
+    The identifier can either be a stringified UUIDv4, a truncated portion
+    of a UUID or a NCBI Taxonomy ID.
+
+    Raise a PartialIDConflictError if >1 OTU is found.
+    """
+    otu_id = None
+
+    if len(identifier) == 32:
+        try:
+            otu_id = UUID(identifier)
+        except ValueError:
+            otu_id = None
+
+    elif identifier.isnumeric():
+        try:
+            taxid = int(identifier)
+        except ValueError:
+            pass
+        else:
+            otu_id = repo.get_otu_id_by_taxid(taxid)
+
+    else:
+        try:
+            otu_id = repo.get_otu_id_by_partial(identifier)
+
+        except PartialIDConflictError:
+            click.echo(
+                "Partial ID too short to narrow down results.",
+                err=True,
+            )
+
+        except InvalidInputError as e:
+            click.echo(
+                e,
+                err=True,
+            )
 
     if otu_id is None:
         click.echo("OTU not found.", err=True)
@@ -26,32 +59,3 @@ def get_otu_from_identifier(repo: Repo, identifier: str) -> RepoOTU:
         sys.exit(1)
 
     return otu_
-
-
-def _get_otu_id_from_other_identifier(repo: Repo, identifier: str) -> UUID | None:
-    """Return an OTU id from the repo if identifier matches a single OTU.
-    Return None if no matching OTU is found, raise a ValueError if >1 OTU is found.
-    """
-    try:
-        taxid = int(identifier)
-    except ValueError:
-        pass
-    else:
-        return repo.get_otu_id_by_taxid(taxid)
-
-    try:
-        return repo.get_otu_id_by_partial(identifier)
-
-    except PartialIDConflictError:
-        click.echo(
-            "Partial ID too short to narrow down results.",
-            err=True,
-        )
-
-    except InvalidInputError as e:
-        click.echo(
-            e,
-            err=True,
-        )
-
-    return None
