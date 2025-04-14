d1cbf522520fd5565eb6424b61be4e2849a34bd6
Shelley Gao <syugao@gmail.com>
2025-04-14 14:04:26 -0700

    check_otu_is_valid uses model_validate

--- ref_builder/otu/validate.py
+++ ref_builder/otu/validate.py
@@ -12,23 +12,25 @@ logger = get_logger("otu.validate")
 def check_otu_is_valid(unvalidated_otu: RepoOTU) -> bool:
     """Assure that an OTU can pass the validation standard."""
     try:
-        OTU(
-            id=unvalidated_otu.id,
-            legacy_id=unvalidated_otu.legacy_id,
-            name=unvalidated_otu.name,
-            taxid=unvalidated_otu.taxid,
-            acronym=unvalidated_otu.acronym,
-            molecule=unvalidated_otu.molecule,
-            plan=unvalidated_otu.plan,
-            excluded_accessions=unvalidated_otu.excluded_accessions,
-            representative_isolate=unvalidated_otu.representative_isolate,
-            isolates=unvalidated_otu.isolates,
+        OTU.model_validate(
+            {
+                "id": unvalidated_otu.id,
+                "legacy_id": unvalidated_otu.legacy_id,
+                "name": unvalidated_otu.name,
+                "taxid": unvalidated_otu.taxid,
+                "acronym": unvalidated_otu.acronym,
+                "molecule": unvalidated_otu.molecule,
+                "plan": unvalidated_otu.plan,
+                "excluded_accessions": unvalidated_otu.excluded_accessions,
+                "isolates": unvalidated_otu.isolates,
+                "representative_isolate": unvalidated_otu.representative_isolate,
+            }
         )
 
     except ValidationError as exc:
         logger.warning(
             "OTU does not pass validation.",
-            id=unvalidated_otu.id,
+            id=str(unvalidated_otu.id),
             name=unvalidated_otu.name,
             taxid=unvalidated_otu.taxid,
         )
@@ -54,7 +56,10 @@ def otu_error_handler(e: ValidationError):
         if not error["loc"]:
             if error["type"] in ("sequence_too_short", "sequence_too_long"):
                 error["loc"] = (
-                    f"isolates[{error['ctx'].get('isolate_id')}]",
-                    f"sequences[{error['ctx'].get('sequence_id', '')}]"),
+                    (
+                        f"isolates[{error['ctx'].get('isolate_id')}]",
+                        f"sequences[{error['ctx'].get('sequence_id', '')}]",
+                    ),
+                )
 
     return new_errors
